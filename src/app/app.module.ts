5c3e8b22c6f9338c380c482162eb39d238dd0088
sharonandisi <sharonandisi.28@gmail.com>
2018-10-19 12:37:53 +0300

    create form layout on quotes-form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { QuotesFormComponent } from './quotes-form/quotes-form.component';
+import { QuoteComponent } from './quote/quote.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuotesFormComponent,
+    QuoteComponent
   ],
   imports: [
     BrowserModule
